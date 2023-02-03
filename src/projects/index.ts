ccfb7c3e62de8d3e04bde36f27cdf147ed82e72c
GiedriusT <garoslavas@gmail.com>
2023-02-03 18:50:41 +0100

    Add thumbs and videos for Shil and Dialrhea

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -2,7 +2,11 @@ import socialSequencerMd from './social-sequencer/social-sequencer.md';
 import socialSequencerThumb from './social-sequencer/social-sequencer.jpg';
 import socialSequencerVideo from './social-sequencer/social-sequencer.mp4';
 import shilMd from './shil/shil.md';
+import shilThumb from './shil/shil.jpg';
+import shilVideo from './shil/shil.mp4';
 import dialrheaMd from './dialrhea/dialrhea.md';
+import dialrheaThumb from './dialrhea/dialrhea.jpg';
+import dialrheaVideo from './dialrhea/dialrhea.mp4';
 import { Project } from '../types';
 
 const projects: Project[] = [
@@ -19,12 +23,16 @@ const projects: Project[] = [
     title: 'Shil',
     subtitle: 'video game with unique art style',
     article: shilMd,
+    thumb: shilThumb,
+    video: shilVideo,
   },
   {
     slug: 'dialrhea',
     title: 'Dialrhea',
     subtitle: 'revolutionary shitty machine',
     article: dialrheaMd,
+    thumb: dialrheaThumb,
+    video: dialrheaVideo,
   },
 ];
 
