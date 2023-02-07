20829d7535aab7cd05303c15c0a192ebcc54d5b1
GiedriusT <garoslavas@gmail.com>
2023-02-07 11:12:01 +0100

    Decrease hero section height

--- src/components/Hero/Hero.styles.ts
+++ src/components/Hero/Hero.styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const HeroContainer = styled.div`
-  height: 60vh;
+  height: 50vh;
   display: flex;
   justify-content: center;
   align-items: center;
