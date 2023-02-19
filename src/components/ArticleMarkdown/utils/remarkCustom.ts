ec5e8fbc2d1b77b86d8771297c9bea4be2fc37b8
GiedriusT <garoslavas@gmail.com>
2023-02-20 00:48:38 +0100

    Refactor Remark plug-in code a bit

--- src/components/ArticleMarkdown/utils/remarkCustom.ts
+++ src/components/ArticleMarkdown/utils/remarkCustom.ts
@@ -12,24 +12,38 @@ interface AstLinkNode extends AstNode {
   url: string;
 }
 
+const isYoutubeLink = (node: AstLinkNode) => {
+  return node.url.indexOf('https://www.youtube.com/') === 0;
+};
+
+const isYoutubeImageLink = (node: AstLinkNode) => {
+  return isYoutubeLink(node)
+    && node.children?.findIndex((child) => child.type === 'image') !== -1;
+};
+
+const isYoutubeTextLink = (node: AstLinkNode) => {
+  return isYoutubeLink(node)
+    && node.children?.findIndex((child) => child.type === 'image') === -1
+    && node.children?.findIndex((child) => child.type === 'text') !== -1;
+};
+
 const processLink = (node: AstLinkNode) => {
   console.log(node);
-  if (node.url.indexOf('https://www.youtube.com/') === 0) {
-    if (node.children?.find((child) => child.type === 'image')) {
-      node.type = 'text';
-      node.value = '';
-      return;
-    }
+  if (isYoutubeTextLink(node)) {
     const url = node.url.replace('watch?v=', 'embed/');
     const title = node.children?.find((child) => child.type === 'text')?.value || 'YouTube Video Player';
     node.type = 'html';
     node.value = `<iframe width="560" height="315" src="${url}" title="${title}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     delete node.children;
+  } else if (isYoutubeImageLink(node)) {
+    node.type = 'text';
+    node.value = '';
+    delete node.children;
   }
 };
 
+// Written by following: https://swizec.com/blog/how-to-build-a-remark-plugin-to-supercharge-your-static-site/
 const remarkCustom = () => {
-  // return function transformer(tree: Node, file) {
   return function transformer(tree: AstNode) {
     visit(tree, 'link', processLink);
   };
