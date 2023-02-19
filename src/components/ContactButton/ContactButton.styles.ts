6bdd74453335ce8a6bbe9e9afdec3aaf109e2007
GiedriusT <garoslavas@gmail.com>
2023-02-19 20:22:19 +0100

    Add animation to contact button

--- src/components/ContactButton/ContactButton.styles.ts
+++ src/components/ContactButton/ContactButton.styles.ts
@@ -5,11 +5,13 @@ export const ContactButtonStyled = styled.button`
   border-radius: 100px;
   border: 0;
   padding: 11px 33px;
-  background-color: rgba(33, 150, 243, 1);
   color: white;
   cursor: pointer;
+  background-color: rgba(33, 150, 243, 1);
+  transition: background-color 0.17s ease-out;
 
   &:hover {
     background-color: rgba(33, 150, 243, 0.7);
+    transition: background-color 0.17s ease-out;
   }
 `;
