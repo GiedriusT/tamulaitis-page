cd0e4e2abd82c5ec3bee444873b27411276fef3e
GiedriusT <garoslavas@gmail.com>
2023-02-21 12:01:09 +0100

    Add support for multiple markdown file generation

--- src/utils/mdgen.ts
+++ src/utils/mdgen.ts
@@ -1,14 +1,27 @@
 import fs from 'fs';
 import { SITE_URL, CONTACT_EMAIL, MY_NAME, TAGLINE, WHO_I_AM, WHAT_I_DO } from '../constants';
 
-const template = fs.readFileSync('./src/templates/README.template.md', 'utf-8');
-const output = template
-  .replaceAll('{{MY_NAME}}', MY_NAME)
-  .replaceAll('{{TAGLINE}}', TAGLINE)
-  .replaceAll('{{WHO_I_AM}}', WHO_I_AM)
-  .replaceAll('{{WHAT_I_DO}}', WHAT_I_DO)
-  .replaceAll('{{CONTACT_EMAIL}}', CONTACT_EMAIL)
-  .replaceAll('{{SITE_URL}}', SITE_URL);
-fs.writeFileSync('./README.md', output);
+const MARKDOWN_FILES = [
+  {
+    templateFilename: './src/templates/README.template.md',
+    outputFilename: './README.md',
+  },
+];
 
-console.log('Markdown files generated!');
+const processTemplate = (template: string): string => {
+  return template
+    .replaceAll('{{MY_NAME}}', MY_NAME)
+    .replaceAll('{{TAGLINE}}', TAGLINE)
+    .replaceAll('{{WHO_I_AM}}', WHO_I_AM)
+    .replaceAll('{{WHAT_I_DO}}', WHAT_I_DO)
+    .replaceAll('{{CONTACT_EMAIL}}', CONTACT_EMAIL)
+    .replaceAll('{{SITE_URL}}', SITE_URL);
+};
+
+for (const { templateFilename, outputFilename } of MARKDOWN_FILES) {
+  const template = fs.readFileSync(templateFilename, 'utf-8');
+  const output = processTemplate(template);
+  fs.writeFileSync(outputFilename, output);
+}
+
+console.log(`Markdown files ${MARKDOWN_FILES.map(obj => `'${obj.outputFilename}'`).join(', ')} generated!`);
