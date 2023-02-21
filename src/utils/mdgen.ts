10df99e6a38e92726a906f30753969655727273b
GiedriusT <garoslavas@gmail.com>
2023-02-21 11:48:54 +0100

    Fix import issues in CLI scripts

--- src/utils/mdgen.ts
+++ src/utils/mdgen.ts
@@ -1,11 +1,5 @@
 import fs from 'fs';
-
-export const SITE_URL = 'https://tamulaitis.lt';
-export const CONTACT_EMAIL = 'giedrius@tamulaitis.lt';
-export const MY_NAME = 'Giedrius Tamulaitis';
-export const TAGLINE = 'exploring the sweet spot where art and technology collides';
-export const WHO_I_AM = 'Fusing the power of technology and the beauty of music, creating a harmonious blend of innovation and artistry.';
-export const WHAT_I_DO = 'As a musician, I have been honing my craft and making a name for myself in the industry. My unique blend of genres, including rock, pop, and electronic, creates a sound that is both fresh and familiar. I have a natural talent for writing catchy hooks and memorable melodies which has helped me to gain a dedicated fan base. I pride myself on my creative approach to music and my undeniable stage presence. I am determined to continue making waves in the industry and am excited for what the future holds.';
+import { SITE_URL, CONTACT_EMAIL, MY_NAME, TAGLINE, WHO_I_AM, WHAT_I_DO } from '../constants';
 
 const template = fs.readFileSync('./src/templates/README.template.md', 'utf-8');
 const output = template
