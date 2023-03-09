1d0894d704b1c92a87931a94c59c1ea867b16ee3
GiedriusT <garoslavas@gmail.com>
2023-03-09 18:01:30 +0100

    Change Shil subtitle

--- src/projects/shil/index.ts
+++ src/projects/shil/index.ts
@@ -3,7 +3,7 @@ import { Project } from '../../types';
 const project: Project = {
   slug: 'shil',
   title: 'Shil',
-  subtitle: 'video game with unique art style',
+  subtitle: 'unique game in monochrome world',
 };
 
 export default project;
