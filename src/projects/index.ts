e2b179f6f1660bd9bb426c4b54912e07e88c731c
GiedriusT <garoslavas@gmail.com>
2023-03-10 15:18:06 +0100

    Fix project import order

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,6 +1,6 @@
 import { Project } from '../types';
 import { default as socialSequencer } from './social-sequencer';
-import { default as shil } from './shil';
+import { default as kamchatkaVinyl } from './kamchatka-vinyl';
 import { default as dialrhea } from './dialrhea';
 import { default as skraidantisRadijas } from './skraidantis-radijas';
 import { default as sewers } from './sewers';
@@ -8,7 +8,7 @@ import { default as moonLove } from './moon-love';
 import { default as wavetwistersVr } from './wavetwisters-vr';
 import { default as captainBadass } from './captain-badass';
 import { default as bembisBembioMotina } from './bembis-bembio-motina';
-import { default as kamchatkaVinyl } from './kamchatka-vinyl';
+import { default as shil } from './shil';
 
 const projects: Project[] = [
   socialSequencer,
