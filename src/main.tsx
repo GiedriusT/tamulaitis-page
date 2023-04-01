f43f101d56adbd83da07375a7d140c053eb40265
GiedriusT <garoslavas@gmail.com>
2023-04-01 14:51:25 +0200

    Impelemt GoogleAnalytics properly

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-WBHFV19M7R');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
