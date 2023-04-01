0e17f43bd19d05a6c2c388de4172bc6fd830c2fb
GiedriusT <garoslavas@gmail.com>
2023-04-01 14:54:29 +0200

    Add action to contact button

--- src/components/ContactButton/ContactButton.tsx
+++ src/components/ContactButton/ContactButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as S from './ContactButton.styles';
 
 const ContactButton: React.FC = () => (
-  <S.ContactButtonStyled>Contact Me</S.ContactButtonStyled>
+  <S.ContactButtonStyled onClick={() => window.location.href = 'mailto:giedrius@tamulaitis.lt'}>Contact Me</S.ContactButtonStyled>
 );
 
 export default ContactButton;
