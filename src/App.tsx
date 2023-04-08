6bf923feaa27772d62c95f58f2108baa5a8245c3
GiedriusT <garoslavas@gmail.com>
2023-04-08 10:06:37 +0200

    Add scroll to top behaviour on route change

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Layout from './components/Layout/Layout';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import { HelmetProvider } from 'react-helmet-async';
+import { ScrollToTop } from './components';
 
 const App: React.FC = () => {
   return (
     <HelmetProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Layout>
           <AppRoutes />
         </Layout>
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as ProjectList } from './ProjectList/ProjectList';
 export { default as ContactButton } from './ContactButton/ContactButton';
 export { default as Hero } from './Hero/Hero';
 export { default as ArticleMarkdown } from './ArticleMarkdown/ArticleMarkdown';
+export { default as ScrollToTop } from './ScrollToTop/ScrollToTop';
