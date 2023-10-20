e02200ed1def974a73ec091a5330d9d9d62dabfe
GiedriusT <garoslavas@gmail.com>
2023-10-20 18:31:39 +0200

    Remove vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, Plugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import eslint from 'vite-plugin-eslint';
 import { visualizer } from 'rollup-plugin-visualizer';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -11,7 +10,6 @@ const PREVIEW_SERVER_PORT = parseInt(process.env.PREVIEW_SERVER_PORT || '4173', 
 export default defineConfig({
   plugins: [
     react(),
-    eslint(),
     visualizer({
       filename: 'bundle-stats.html',
     }) as unknown as Plugin,
