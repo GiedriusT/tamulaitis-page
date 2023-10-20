42d3adebcada2182f34923f7b0b801854a3f2bd0
GiedriusT <garoslavas@gmail.com>
2023-10-20 18:27:47 +0200

    Configure ESLint properly

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig, PluginOption } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
 import { visualizer } from 'rollup-plugin-visualizer';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,9 +11,10 @@ const PREVIEW_SERVER_PORT = parseInt(process.env.PREVIEW_SERVER_PORT || '4173', 
 export default defineConfig({
   plugins: [
     react(),
+    eslint(),
     visualizer({
       filename: 'bundle-stats.html',
-    }) as PluginOption,
+    }) as unknown as Plugin,
   ],
   assetsInclude: ['**/*.md'],
   preview: {
