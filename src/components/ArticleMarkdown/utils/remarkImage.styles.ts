b954e27c1d58ce1d5c3d97e5a45a3f23462ea181
GiedriusT <giedrius.tamulaitis@gmail.com>
2023-11-13 19:43:34 +0100

    Fix full width image issue

--- src/components/ArticleMarkdown/utils/remarkImage.styles.ts
+++ src/components/ArticleMarkdown/utils/remarkImage.styles.ts
@@ -3,7 +3,7 @@ import { css } from 'styled-components';
 // eslint-disable-next-line import/prefer-default-export
 export const remarkImageStyles = css`
   img.full-width {
-    max-width: 100%;
+    max-width: none;
   }
 
   img.padding-on-mobile {
