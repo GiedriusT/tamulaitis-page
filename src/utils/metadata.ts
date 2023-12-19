52c681591f23ba7eda5141fad77a2ab9d7355914
Giedrius <garoslavas@gmail.com>
2023-12-19 18:25:26 +0200

    Change page title format

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -5,6 +5,6 @@ const concatenateTitle = (segments: string[]) => segments.join(' - ');
 
 export const getIndexPageTitle = () => concatenateTitle([MY_NAME, TAGLINE]);
 export const getProjectPageTitle = (project: Project) => (
-  concatenateTitle([project.title, project.subtitle, MY_NAME])
+  `${concatenateTitle([project.title, project.subtitle])} (by ${MY_NAME})`
 );
 export const getSecretPageTitle = () => concatenateTitle([SECRET_PAGE_TITLE, MY_NAME]);
